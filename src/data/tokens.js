c7d17ab2b44c705b402a941198634989eff90dbb
Khripkov Sergey <porosenk@gmail.com>
2024-12-03 17:29:48 +0300

    Добавлен поиск существ и токенов

---
+++ src/data/tokens.js
@@ -0,0 +1,18 @@
+export const tokens = [
+    { enname: "Неофициальные токены", link: "/Collection/Tokens/Custom", img: "/images/scryfall/tokens/goblin.jpg" },
+    { enname: "Goblin", link: "/Collection/Tokens/goblin-tugl", img: "/images/scryfall/tokens/goblin-tugl.jpg" },
+    { enname: "Goblin Soldier", link: "/Collection/Tokens/goblin_soldier-mpr", img: "/images/scryfall/tokens/goblin-soldier-mpr.jpg" },
+    { enname: "Goblin", link: "/Collection/Tokens/goblin_1_1_P03", img: "/images/scryfall/tokens/goblin-p03.jpg" },
+    { enname: "Goblin Rogue", link: "/Collection/Tokens/goblin_rogue-tlrw", img: "/images/scryfall/tokens/goblin-rogue-tlrw.jpg" },
+    { enname: "Goblin Warrior", link: "/Collection/Tokens/goblin_warrior-tshm", img: "/images/scryfall/tokens/goblin-warrior-tshm.jpg" },
+    { enname: "Tuktuk the Returned", link: "/Collection/Tokens/tuktuk_the_returned-troe", img: "/images/scryfall/tokens/tuktuk-the-returned-troe.jpg" },
+    { enname: "Goblin", link: "/Collection/Tokens/goblin-tcn2", img: "/images/scryfall/tokens/goblin-tcn2.jpg" },
+    { enname: "Goblin", link: "/Collection/Tokens/goblin-tc16", img: "/images/scryfall/tokens/goblin-tc16.jpg" },
+    { enname: "Goblin", link: "/Collection/Tokens/goblin-tgk2", img: "/images/scryfall/tokens/goblin-tgk2.jpg" },
+    { enname: "Goblin Wizard", link: "/Collection/Tokens/goblin_wizard-tm21", img: "/images/scryfall/tokens/goblin-wizard-tm21.jpg" },
+    { enname: "Goblin Construct", link: "/Collection/Tokens/goblin_construct-tznr", img: "/images/scryfall/tokens/goblin-construct-tznr.jpg" },
+    { enname: "Goblin Shaman", link: "/Collection/Tokens/goblin_shaman-tneo", img: "/images/scryfall/tokens/goblin-shaman-tneo.jpg" },
+    { enname: "Phyrexian Goblin", link: "/Collection/Tokens/phyrexian_goblin-tone", img: "/images/scryfall/tokens/phyrexian-goblin-tone.jpg" },
+    { enname: "Goblin", link: "/Collection/Tokens/goblin-trvr", img: "/images/scryfall/tokens/goblin-trvr.jpg" },
+    { enname: "Test", link: "/Collection/Tokens/goblin-trvr", img: "/images/scryfall/tokens/goblin-trvr.jpg" }
+];
